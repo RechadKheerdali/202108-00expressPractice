79de717e3b422a464c6b238315a906bf05bb5805
ik <rechad.kheerdali@gmail.com>
2021-08-12 17:23:29 +0100

    minor updates on ik7.js

--- ik7.js
+++ ik7.js
@@ -9,9 +9,9 @@ const iKapp = iKexpress();
 iKapp.use( iKcookieParser() )
 
 //You can think of this as mock database
-const iKuser = {
+const iKuserDb = {
     iKname: 'rechad',
-    Age: 75
+    iKage: 75
 }
 
 //No cookie set yet
@@ -19,20 +19,20 @@ iKapp.get('/', (req, res) => {
     res.send('iK Rechad Kheerdali');
 });
 
-//cookie is created. You can think of this as signing/logging in
+//cookie is created. You can think of this as signing/login routes
 iKapp.get('/iksetuser', (req, res) => {
-    res.cookie( 'iKuserData', iKuser )
-    res.send('iKuserData add to cookie');
+    res.cookie( 'iKuserDbData', iKuserDb )
+    res.send('iKuserDbData add to cookie');
 });
 
 //Accessing the cookie data. Possible cookie authentication could be done here
 iKapp.get('/ikgetuser', (req, res) => {
-    res.send(req.cookies.iKuserData);
+    res.send(req.cookies.iKuserDbData);
 });
 
 //Clear cookie
 iKapp.get('/ikclearcookie', (req, res) => {
-    res.clearCookie('iKuserData')
+    res.clearCookie('iKuserDbData')
     res.send('iK cleared cookie')
 });
 
@@ -42,5 +42,5 @@ iKapp.listen(3000, () => console.log('iK server on port 3000'));
 
 /**
  * iK? from this experiment, I notice the cookie was stored in the browser cookier storage and it you can see the data, but with express-session the data looked hashed. Perhaps I would need to do extra settings if I was it to be encrpted
- * 
+ * However it will only work with express app ssr and not csr as the cookie will not be stored on the clientside. Perhaps you need to use the same domain url
  */

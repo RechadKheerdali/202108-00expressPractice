258da74c5dac9e585355f97285e597e4ada14769
ik <rechad.kheerdali@gmail.com>
2021-08-12 07:52:29 +0100

    completed ik17.js on Joi validation

--- ik17.js
+++ ik17.js
@@ -1,32 +1,33 @@
-/**     INCOMPLETE   (iK I think I completed it but come back after you are happy)
+/**
  * Joi  (validation error handling)
  */
+
 const iKjoi = require('joi');
 
-const iKmockData = {
-    iKname: 'rechad',
-    iKemail: 'rechadkheerdali@gmail.com',
-    iKpassword: '123',
-};
-
-const iKschema = iKjoi.object().keys({
-    iKname: iKjoi.string().trim().required(),
-    iKemail: iKjoi.string().trim().email().required(),
-    iKpassword: iKjoi.string().min(5).max(10).required()
-})
-
-// console.log(
-//     iKschema.validate( iKmockData )
-// )
-async function ik() {
-    try {
-        await iKschema.validateAsync( iKmockData );
+const iKmockDataOfArrayObject = [
+    {
+        iKname: 'rechad',
+        iKemail: 'rechadkheerdali@gmail.com',
+        iKpassword: 'richard',
     }
-    catch (err) { 
-        console.error(err.details[0].message)
-    }
-}
-ik()
+];
+
+//Your validation schema condition required to pass
+const iKschema = iKjoi.array().items(
+    iKjoi.object({
+        iKname: iKjoi.string().trim().required(),
+        iKemail: iKjoi.string().trim().email().required(),
+        iKpassword: iKjoi.string().min(5).max(10).required()
+    })
+)
+
+//Use .validate() to validate any data you want to match the schema
+const iKvalidate = iKschema.validate( iKmockDataOfArrayObject );
+
+console.log(iKvalidate)
+    /*OP: If successful then it will return {value: … } with the mock data inside */
+    /*OP: If data fails validation, then it will return {value: … , error: …} . The error property will have details why it failed */
+
 
 /**
  * 
